08165a2a2af762f371cfa99e6afd6a2bcda96be0
OlegKravinskyy <kravinskyy@gmail.com>
2022-08-18 18:34:51 +0300

    added task-3

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,29 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const navListEl = document.querySelector("ul");
+
+const elements = ingredients.map((option) => {
+  const navItemEl = document.createElement("li");
+  navItemEl.classList.add("item");
+  navItemEl.textContent = option;
+
+  return navItemEl;
+});
+
+navListEl.append(...elements);
+
+//   ingredients.forEach((el) => {
+//   const navItemEl = document.createElement("li");
+//   navItemEl.classList.add("item");
+//   navItemEl.textContent = el;
+//   navListEl.appendChild(navItemEl);
+// });
+
+console.log(navListEl);
