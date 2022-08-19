fcb34f8a04c183e3bb7e6a191486b61932966255
OlegKravinskyy <kravinskyy@gmail.com>
2022-08-19 12:47:33 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+console.log(form);
+const user = {};
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (!email.value || !password.value) {
+    return alert("Error");
+  }
+
+  user.email = email.value;
+  user.password = password.value;
+  console.log(user);
+  event.currentTarget.reset();
+}
