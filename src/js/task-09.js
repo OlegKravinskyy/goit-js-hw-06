db2fbf756df826e9277e7c59791755c75e3967c1
OlegKravinskyy <kravinskyy@gmail.com>
2022-08-19 13:21:42 +0300

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+button.addEventListener("click", onChangeBodyColor);
+
+function onChangeBodyColor(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+}
