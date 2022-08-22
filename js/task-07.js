d25f8b631b1d2b1242d4d3de4f0d1698df004113
OlegKravinskyy <kravinskyy@gmail.com>
2022-08-23 00:56:39 +0300

    fixed task-7

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const input = document.querySelector("#font-size-control");
 const changeSizeText = document.querySelector("#text");
+changeSizeText.style.fontSize = `${input.value}px`;
 
 console.log(input.value);
 input.addEventListener("input", onChangeText);
