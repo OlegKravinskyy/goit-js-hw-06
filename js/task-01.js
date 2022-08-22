3192241b3df88c937bb9890974cdd5f2810cce9f
OlegKravinskyy <kravinskyy@gmail.com>
2022-08-23 00:44:29 +0300

    fixed task-06

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,8 @@ const navItemCategory = document.querySelectorAll(".item");
 console.log("Number of categories: " + navItemCategory.length);
 
 navItemCategory.forEach((el) => {
-  const catagoryName = el.querySelector("h2");
+  const catagoryName = el.firstElementChild("h2");
   console.log("Category: " + catagoryName.textContent);
-  const navCategoryLength = el.querySelectorAll("li");
+  const navCategoryLength = el.lastElementChild("li");
   console.log("Elements: " + navCategoryLength.length);
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,18 @@
 const input = document.querySelector("#validation-input");
 const inputClass = input.classList;
+const inputDataLength = Number(input.dataset.length);
 
 input.addEventListener("blur", onInputFocus);
 
 function onInputFocus(event) {
   const text = event.currentTarget.value;
-  if (text.length <= 6) {
-    inputClass.add("valid");
-  } else {
+  console.log(text.length);
+
+  if (text.length !== inputDataLength) {
     inputClass.add("invalid");
+    inputClass.remove("valid");
+  } else {
+    inputClass.add("valid");
+    inputClass.remove("invalid");
   }
 }
