1e885d037a595c9dc6abc7cebfb860bdd7f27490
OlegKravinskyy <kravinskyy@gmail.com>
2022-08-24 09:26:11 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divBox = document.querySelector("#boxes");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const input = document.querySelector("#controls").firstElementChild;
+
+console.log(input);
+console.log(btnCreate);
+console.log(btnDestroy);
+
+btnCreate.addEventListener("click", onCreateDiv);
+btnDestroy.addEventListener("click", onDestroyDiv);
+
+function onCreateDivElements(amount) {
+  let size = 30;
+  let markup = "";
+
+  for (let i = 0; i < amount; i += 1) {
+    const divColor = getRandomHexColor();
+    markup += `<div style="width:${size + i * 10}px; height:${
+      size + i * 10
+    }px; background-color: ${divColor}; display:block;"> </div>`;
+  }
+  divBox.insertAdjacentHTML("afterbegin", markup);
+}
+
+function onCreateDiv() {
+  const amount = Number(input.value);
+
+  onCreateDivElements(amount);
+}
+
+function onDestroyDiv() {
+  divBox.innerHTML = "";
+}
